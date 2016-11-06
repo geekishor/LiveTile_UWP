ea0ad19088d4e5ab8de6f3ddb57677425ec2bb80
Procit BV <Procit BV>
2016-11-06 20:09:47 +0545

    Project added

---
+++ Live Tiles UWP/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Live_Tiles_UWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileWide310x150Text01);
+            var tileAttributes = tileXml.GetElementsByTagName("text");
+            tileAttributes[0].AppendChild(tileXml.CreateTextNode("This is a UWP Tile message"));
+            var tileNotification = new TileNotification(tileXml);
+            TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+
+            UpdateBadgeTile();  //For Updating Badge Number 
+        }
+
+        private void UpdateBadgeTile()
+        {
+            var type = BadgeTemplateType.BadgeNumber;
+            var xml = BadgeUpdateManager.GetTemplateContent(type);
+
+            var elements = xml.GetElementsByTagName("badge");
+            var element = elements[0] as Windows.Data.Xml.Dom.XmlElement;
+            element.SetAttribute("value", "7");
+
+            var badgeUpdator = BadgeUpdateManager.CreateBadgeUpdaterForApplication();
+            var notif = new BadgeNotification(xml);
+            badgeUpdator.Update(notif);
+        }
+    }
+}
